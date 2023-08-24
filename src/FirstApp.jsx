cd808f10e2241912809437f1cea18ea4c879f48b
Juan Manuel Carranza <ikkicarranza@gmail.com>
2023-08-24 18:16:20 -0400

    Transformacion a un ejemplo de contador basico.

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -2,9 +2,40 @@
 //     return (<h1>Hola Mundo!!</h1>);
 // }
 // export default App;
+//import { Fragment } from "react"
+// function ReturnName(newMessage1) {
+//   var message=newMessage1.message;
+//   return message;
+  
+// }
+// const newMessage = {
+//   message: 'Hola Ikki',
+//   title: 'Fernando'
+// };
 
-export const FirstApp = () => {
+import PropTypes from 'prop-types';
+
+export const FirstApp = ( {title, subtitle, name}) => {
+ //console.log(props);
+
+
   return (
-    <div>Hello World</div>
+    <>
+    <h1>{ title }</h1>
+    <p>Soy un subtitulo {subtitle}</p>
+    <p>{name}</p>
+      </>
+
   )
+}
+
+FirstApp.propTypes ={
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps ={
+  title: 'No hay titulo',
+  subtitle: 'No hay subtitulo',
+  name: 'Juan Carranza'
 }
