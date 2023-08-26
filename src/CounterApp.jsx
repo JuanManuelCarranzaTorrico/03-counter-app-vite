a7b8f93274d48443e777a7d7e92b359a9aa130bf
Juan Manuel Carranza <ikkicarranza@gmail.com>
2023-08-26 10:52:07 -0400

    Final Ejemplo 1

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -12,14 +12,20 @@ export const CounterApp = ({value}) => {
         setCounter(counter+1)
         //setCounter( (c) => c+1 )
         }
+    const handleLess=()=>{
+      setCounter(counter-1);
+    }
+    const handleReset=()=>{
+      setCounter(value);
+    }
   return (
     <>
     <h1>CounterApp</h1>
     <h2>{ counter }</h2>
 
-    <button onClick={  handleAdd }>
-        +1
-    </button>
+    <button onClick={  handleAdd }>+1</button>
+    <button onClick={handleLess }>-1</button>
+    <button onClick={handleReset} >Reset</button>
     </>
   )
 }
